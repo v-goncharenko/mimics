78da00c605dda758e88b82e3571df04aa1c26054
V G <vladislav.goncharenko@phystech.edu>
2020-04-12 14:10:01 +0300

    added digital filter to default pipeline

--- mimics/transformers/__init__.py
+++ mimics/transformers/__init__.py
@@ -17,15 +17,14 @@ from .transformers import (
 
 
 def get_preprocessing(
-    points: str = 'brows',
-    *,
-    steps: Optional[int] = None,
-    window_length: int = 7,  # emphirical default for 30 fps
+    points: str = 'brows', *, steps: Optional[int] = None,
 ):
     '''Makes preprocessing pipeline for face shapes
 
     Args:
-        steps: number of setps of final transform to make (useful for debugging and visualising)
+        points: which points to center on and restrict output to
+        steps: number of setps of final transform to make from 0 to max preprocessors
+            (useful for debugging and visualising)
     '''
     if steps == 0:
         return make_pipeline(Identical())
@@ -35,8 +34,8 @@ def get_preprocessing(
         EyesRotator(ex.inds_68['left_eye'], ex.inds_68['right_eye']),
         Scaler(ex.inds_68['left_eye'], ex.inds_68['right_eye'], axis=0),
         Scaler(ex.inds_68['eyes'], ex.inds_68['lips'], axis=1),
+        ButterFilter(30.0, 0.1, 3.0, 4, preserve_mean=True),
         Centerer(ex.inds_68[points]),
-        Smoother(window_length, 'hamming'),
         ChannelsSelector(ex.inds_68[points]),
     )
     return make_pipeline(*transformers[:steps])
